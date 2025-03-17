86b41a9e6efacc9617e3522e5b9ad9a48bbcbbcd
richtda4 <richtda4@fit.cvut.cz>
2025-03-17 03:27:48 +0100

    Main class that helps test classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "Window.h"
-#include "Vulkan/Config/VulkanManager.h"
+#include "Vulkan/Instance.h"
+#include "Vulkan/PhysicalDevice.h"
+#include "Vulkan/Device.h"
+#include "Vulkan/Surface.h"
+#include "Vulkan/Swapchain.h"
+#include "Vulkan/Shader.h"
+#include "Vulkan/Pipeline.h"
 int main(){
-    FLUDER::Window window;
-    FLUDER::Vulkan::VulkanManager configuration(window,true);
-    while(!window.shouldClose())
-     	configuration.draw();
+    fldr::Window window;
+    fldr::vk::Instance instance(true);
+    fldr::vk::Device device(instance,true);
+    fldr::vk::Surface surface(instance, window);
+    fldr::vk::Shader vertexShader(device, "shader.vert");
+    fldr::vk::Shader fragmentShader(device, "shader.frag");
+    fldr::vk::Pipeline pipeline(device, surface, vertexShader, fragmentShader);
     return 0;
+
 }

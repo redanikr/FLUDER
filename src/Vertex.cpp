51d2577f05e79aededbdd50b6a83a2b058a22982
richtda4 <richtda4@fit.cvut.cz>
2025-03-21 23:30:39 +0100

    Add Vertex class

---
+++ src/Vertex.cpp
@@ -0,0 +1,3 @@
+#include "Vertex.h"
+
+fldr::Vertex::Vertex(float x, float y, float z) : m_x(x), m_y(y), m_z(z) {}
---
+++ src/Vertex.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace fldr{
+    class Vertex{
+        public:
+        Vertex(float x, float y, float z);
+        private:
+        float m_x;
+        float m_y;
+        float m_z;
+    };
+}
